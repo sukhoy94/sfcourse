263a4493001a038751b661f7d7b932a8f75d15d3
andrii <andrii.sukhoi@nocowanie.pl>
2020-12-30 12:09:23 +0100

    validation

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -9,6 +9,7 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -28,11 +29,14 @@ class ArticleFormType extends AbstractType
             ->add('title', TextType::class, [
                 'help' => 'add title'
             ])
-            ->add('body')
+            ->add('body', TextareaType::class, [
+                'required' => true,
+            ])
             ->add('author_name', EntityType::class, [
                 'class' => User::class,
                 'placeholder' => 'Choose an author',                
-                'choices' => $this->userRepository->findAllUsersAlphabetically(),              
+                'choices' => $this->userRepository->findAllUsersAlphabetically(),
+                'invalid_message' => 'Symfony is too smart for your hacking!',
             ])
             ->add('created_at', null, [
                 'widget' => 'single_text'
